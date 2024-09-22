61c44983dbb5b2cbb1404e42ae09c20d1f9cbaac
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-09-21 22:42:09 -0400

    Tentando ajustar configuração pro deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HotelList from "./pages/HotelList";
 import HotelDetails from "./pages/HotelDetails";
 import EditHotel from "./pages/EditHotel";
 import AddHotel from "./pages/AddHotel";
 
-
 import "./App.module.css";
-
-
 
 function App() {
   return (
     <>
-      
-      <BrowserRouter>
-      
+      <Router>
         <Routes>
           <Route path="/" element={<HotelList />} />
           <Route path="/add-new-hotel" element={<AddHotel />} />
           <Route path="/hotel/:id" element={<HotelDetails />} />
           <Route path="/edit-hotel/:id" element={<EditHotel />} />
         </Routes>
-      </BrowserRouter>
-      
-
+      </Router>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: "/at-fundamentos-react",
+  server: {
+    historyApiFallback: true
+  },
 })
