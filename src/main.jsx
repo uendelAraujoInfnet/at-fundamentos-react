46fac674704fd5b61564d4c4819cdbe957bdb8fc
uendelAraujoInfnet <uendel.araujo@al.infnet.edu.br>
2024-09-22 12:56:30 -0400

    Correção da routa pro deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/at-fundamentos-react",
-  server: {
-    historyApiFallback: true
-  },
 })
